a70004515361be8a3af23d6ec8e2ffdce768257e
Dinkar Jallan <dinkarjallan@gmail.com>
2024-11-23 22:04:16 +0530

    testing out util working and stuff

---
+++ src/utils/sum.ts
@@ -0,0 +1,9 @@
+export const Sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+// Delete me
+export const Thing = () => {
+  console.log('THING, is done!');
+  return 'this is a thing';
+};
